2d8fc2a47c695193c874486d435e8cac1aed9e03
satrioed <32364936+satrio13@users.noreply.github.com>
2024-12-19 13:50:11 +0700

    Update form_tambah.blade.php

--- resources/views/admin/user/form_tambah.blade.php
+++ resources/views/admin/user/form_tambah.blade.php
@@ -34,7 +34,7 @@
                                 <div class="form-group row">
                                     <label class="col-sm-2 col-form-label">NAMA <span class="text-danger">*</span></label>
                                     <div class="col-sm-5">
-                                        <input type='text' name='nama' maxlength="100" class='form-control required' placeholder='Nama' value="{{ old('nama') }}">
+                                        <input type='text' name='nama' maxlength='50' class='form-control required' placeholder='Nama' value="{{ old('nama') }}">
                                         <small class="text-danger">
                                             {{ ($errors->first('nama')) ? $errors->first('nama') : '' }}
                                         </small>
@@ -70,7 +70,7 @@
                                 <div class="form-group row">
                                     <label class="col-sm-2 col-form-label">EMAIL <span class="text-danger">*</span></label>
                                     <div class="col-sm-5">
-                                        <input type='email' name='email' class='form-control sepasi required' placeholder='Email' value="{{ old('email') }}">
+                                        <input type='email' name='email' class='form-control sepasi required' maxlength='100' placeholder='Email' value="{{ old('email') }}">
                                         <small class="text-danger">
                                             {{ ($errors->first('email')) ? $errors->first('email') : '' }}
                                         </small>
